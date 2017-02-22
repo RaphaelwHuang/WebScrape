11b007ce41792cc14ff338e6bbfdf14d3f9c0303
sunnybpatel <patel.2222@osu.edu>
2017-02-22 17:57:57 -0500

    Added ability to print all position information

--- crawl.rb
+++ crawl.rb
@@ -1,6 +1,5 @@
 require 'mechanize'
 require 'nokogiri'
-
 # Author: Sunny Patel   2/21
 class Scraper
   @@url = 'https://www.jobsatosu.com/postings/search'
@@ -17,29 +16,27 @@ class Scraper
     form.fields.each {|f| puts f.name}
   end
 
-  # Author: Sunny Patel   2/21
-  def get_form
-    @page.forms.first
-  end
-
   # Author Sunny Patel    2/21
-  # Fills in the searchbar on the main posting page
-  def fill_searchbar(query)
+  # Fills in the searchbar on the posting page and submits form
+  # Modifications:
+  # Sunny Patel 2/22: Fixed to switch page to submitted form
+  def search(query)
     form = get_form
     form['query'] = query
+    @page = form.submit
   end
 
   # Author Sunny Patel    2/22
   # Only selects selector expressions
   # Will print the text of all selectors that match search
-  def search_for_selectors(search)
-    @page.search(search).each {|selector| puts selector.text.strip}
-  end
-
-  # Author Sunny Patel    2/22
-  # Prints the position title and its salary for all positions
-  def print_title_and_salary
-    search_for_selectors '//h3 | //td[(((count(preceding-sibling::*) + 1) = 6) and parent::*)]'
+  # expectedCount is a number used to print spaces between blocks of information
+  def search_for_selectors(search, expectedCount)
+    count = 1
+    @page.search(search).each do |selector|
+      puts selector.text.strip
+      puts if count % expectedCount == 0
+      count += 1
+    end
   end
 
   # Author Sunny Patel    2/22
@@ -49,25 +46,59 @@ class Scraper
   end
 
   # Author Sunny Patel    2/22
-  # RETURNS: the next page OR NIL if page does not exist
-  def next_page
-    @page.link_with(:text => /Next/)
-  end
-
-  # Author Sunny Patel    2/22
   # Prints titles and salaries from every page possible
+  # Restores: @page
   def print_all_titles_and_salaries
+    initial_page = @page
     print_title_and_salary
     while link = next_page
       @page = link.click
       print_title_and_salary
     end
+    # restores page to initial page
+    @page = initial_page
   end
 
+  # Author Sunny Patel    2/22
+  # Prints all basic position information from every page possible
+  # Restores: @page
+  def print_all_position_info
+    initial_page = @page
+    print_position_info
+    while link = next_page
+      @page = link.click
+      print_position_info
+    end
+    # restores page to initial page
+    @page = initial_page
+  end
+
+  private
+  # Author Sunny Patel    2/22
+  # Prints the position title and its salary for all positions
+  def print_title_and_salary
+    search_for_selectors('//h3 | //td[(((count(preceding-sibling::*) + 1) = 6) and parent::*)]', 2)
+  end
+
+  # Author Sunny Patel    2/22
+  # Print all info related to a position
+  def print_position_info
+    search_for_selectors('//*[(@id = "search_results")]//tr[(((count(preceding-sibling::*) + 1) = 1) and parent::*)]//td',6)
+  end
+  # Author Sunny Patel    2/22
+  # RETURNS: the next page OR NIL if page does not exist
+  def next_page
+    @page.link_with(:text => /Next/)
+  end
+
+  # Author: Sunny Patel   2/21
+  def get_form
+    @page.forms.first
+  end
 end
 
-
-
+# REMEMBER TO CLEAN UP THIS BOTTOM PORTION
 jobSite = Scraper.new
-# Tell team about selector gadget.
-jobSite.print_all_titles_and_salaries
+
+jobSite.search('senior')
+jobSite.print_all_position_info
