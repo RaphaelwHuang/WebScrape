da30d503f9ff900df805540d1a23c5714e156931
sunnybpatel <patel.2222@osu.edu>
2017-02-23 17:54:24 -0500

    modified methods to return array of informations

--- crawl.rb
+++ crawl.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 # Author: Sunny Patel   2/21
 class Scraper
   @@url = 'https://www.jobsatosu.com/postings/search'
+  @@basicInformationCount = 6
   # Author: Sunny Patel   2/21
   def initialize
     @agent = Mechanize.new
@@ -26,17 +27,34 @@ class Scraper
     @page = form.submit
   end
 
+  def location(value)
+    form = get_form
+    form['591'] = value
+    @page = form.submit
+  end
+
   # Author Sunny Patel    2/22
-  # Only selects selector expressions
-  # Will print the text of all selectors that match search
-  # expectedCount is a number used to print spaces between blocks of information
+  # Get the text of all selectors that match search
+  # expectedCount the number of elements expected in 1 subarray
+  # RETURNS: A (super)array of (sub)arrays that contains all the information for 1 position
+  # Modifications:
+  # Sunny Patel 2/22: Returns an array so that the information can be accessed
   def search_for_selectors(search, expectedCount)
     count = 1
+    # Sub array that will hold the information of 1 position
+    position_array = []
+    # Super array that holds information of all positions
+    positions_array = []
     @page.search(search).each do |selector|
-      puts selector.text.strip
-      puts if count % expectedCount == 0
+      position_array << selector.text.strip
+      if count % expectedCount == 0
+        positions_array << position_array
+        position_array = []
+      end
       count += 1
     end
+    # Return super array
+    positions_array
   end
 
   # Author Sunny Patel    2/22
@@ -46,45 +64,39 @@ class Scraper
   end
 
   # Author Sunny Patel    2/22
-  # Prints titles and salaries from every page possible
-  # Restores: @page
-  def print_all_titles_and_salaries
+  # RESTORES: @page
+  # RETURNS: Array that contains subarrays which contain all the basic information for 1 position from all pages
+  def get_position_info
     initial_page = @page
-    print_title_and_salary
+    position_array = get_position_single_page
+    # Loop until there is no next page
     while link = next_page
       @page = link.click
-      print_title_and_salary
+      position_array = position_array + get_position_single_page
     end
     # restores page to initial page
     @page = initial_page
+    position_array
   end
 
-  # Author Sunny Patel    2/22
-  # Prints all basic position information from every page possible
-  # Restores: @page
-  def print_all_position_info
-    initial_page = @page
-    print_position_info
-    while link = next_page
-      @page = link.click
-      print_position_info
+  # Author Sunny Patel    2/23
+  # Prints all the information in a super_array
+  def print_info_array(super_array)
+    puts super_array.class
+    puts
+    super_array.each do |sub_array|
+      sub_array.each {|item| puts item}
+      puts
     end
-    # restores page to initial page
-    @page = initial_page
   end
 
   private
   # Author Sunny Patel    2/22
-  # Prints the position title and its salary for all positions
-  def print_title_and_salary
-    search_for_selectors('//h3 | //td[(((count(preceding-sibling::*) + 1) = 6) and parent::*)]', 2)
+  # Get an array of subarrays of the all the basic information for 1 position from 1 page
+  def get_position_single_page
+    search_for_selectors('//*[(@id = "search_results")]//tr[(((count(preceding-sibling::*) + 1) = 1) and parent::*)]//td', @@basicInformationCount)
   end
 
-  # Author Sunny Patel    2/22
-  # Print all info related to a position
-  def print_position_info
-    search_for_selectors('//*[(@id = "search_results")]//tr[(((count(preceding-sibling::*) + 1) = 1) and parent::*)]//td',6)
-  end
   # Author Sunny Patel    2/22
   # RETURNS: the next page OR NIL if page does not exist
   def next_page
@@ -99,6 +111,3 @@ end
 
 # REMEMBER TO CLEAN UP THIS BOTTOM PORTION
 jobSite = Scraper.new
-
-jobSite.search('senior')
-jobSite.print_all_position_info
