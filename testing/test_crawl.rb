4a748c676140229528d67ffb9d5ddbab24b55e72
Tony Su <su.342@osu.edu>
2017-03-01 03:56:24 -0500

    test file for crawl

---
+++ testing/test_crawl.rb
@@ -0,0 +1,28 @@
+require_relative '../crawl'
+require "test/unit"
+
+class TestCrawl < Test::Unit::TestCase
+
+  # Author Tony Su    2/27
+  def test_init
+    assert_nothing_raised {Scraper.new}
+  end
+
+  # Author Tony Su    2/27
+  def test_get_position_links
+    a = Scraper.new
+    link = a.get_position_links
+    assert_equal(30,link.length, "Expected number of link in a single page: ")
+  end
+
+  # Author Tony Su    2/27
+  def test_get_position_links_02
+    a = Scraper.new
+    link = a.get_position_links
+    link.each do |a|
+      assert_match /https:\/\/www.jobsatosu.com.*/, a ,"The link must be a link to jobsarosu.com"
+    end
+
+  end
+
+end
